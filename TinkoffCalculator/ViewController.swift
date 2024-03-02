3429897ff6685ed22c94f651dca6136ffe4872bd
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2024-03-02 15:09:37 +0500

    Autolayout

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        guard let textButton = sender.currentTitle else { return }
+        
+        print(textButton)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Та-дам!")
     }
-
-
 }
 
