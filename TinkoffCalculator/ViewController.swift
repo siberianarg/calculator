cb2422a6aaffbe02b11604300eb6cf398119ecc8
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2024-03-10 21:14:11 +0500

    implemented calculate

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-enum Operation {
-    case add
-    case substrct
-    case multiply
-    case divde
+enum Operation: String {
+    case add = "+"
+    case substrct = "-"
+    case multiply = "x"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) -> Double {
+        switch self {
+            
+        case .add:
+            return number1 + number2
+        case .substrct:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            return number1 / number2
+        }
+    }
 }
 
 enum CalculationHistoryItem {
@@ -19,35 +33,99 @@ enum CalculationHistoryItem {
     case operation(Operation)
 }
 
-
-
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         guard let textButton = sender.currentTitle else { return }
         
-        label.text = textButton
+        if textButton == "," && label.text?.contains(",") == true {
+            return
+        }
         
-        print(textButton)
+        if label.text == "0" {
+            label.text = textButton
+        } else {
+            label.text?.append(textButton)
+        }
     }
     
     @IBAction func operationButtonPressed(_ sender: UIButton) {
         
-        guard let textButton = sender.currentTitle else { return }
+        guard
+            let textButton = sender.currentTitle,
+            let buttonOperation = Operation(rawValue: textButton)
+        else { return }
         
-        label.text = textButton
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else { return }
         
-        print(textButton)
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        
+        resetLabelText()
+    }
+    
+    @IBAction func calculateButtonPressed() {
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else { return }
+        
+        let result = calculate()
+        
+        label.text = numberFormatter.string(from: NSNumber(value: result))
+        
+        calculationHistory.removeAll()
     }
     
     @IBOutlet weak var label: UILabel!
+    
+    var calculationHistory: [CalculationHistoryItem] = []
+    
+    lazy var numberFormatter: NumberFormatter = {
+        let numberFormatter =  NumberFormatter()
+        numberFormatter.usesGroupingSeparator = false
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+        
+        return numberFormatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("Та-дам!")
+        resetLabelText()
+    }
+    
+    func calculate() -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+            else { break }
+            
+            currentResult = operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
+    }
+    
+    func resetLabelText() {
+        label.text = "0"
     }
 }
 
